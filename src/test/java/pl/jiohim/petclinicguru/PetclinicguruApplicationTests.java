7c031959f9f64436e9292df27e4fe445b6e68b3e
Jiohim <bohdankrasin@gmail.com>
2022-10-07 18:32:03 +0200

    create project

---
+++ src/test/java/pl/jiohim/petclinicguru/PetclinicguruApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jiohim.petclinicguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicguruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
