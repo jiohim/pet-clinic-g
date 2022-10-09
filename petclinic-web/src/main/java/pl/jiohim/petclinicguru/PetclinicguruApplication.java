69538de2bf84697bf746decb252f47307b1384a4
Jiohim <bohdankrasin@gmail.com>
2022-10-09 19:04:17 +0200

    fixing wrong refactoring

---
+++ petclinic-web/src/main/java/pl/jiohim/petclinicguru/PetclinicguruApplication.java
@@ -0,0 +1,12 @@
+package pl.jiohim.petclinicguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicguruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicguruApplication.class, args);
+    }
+}
---
+++ petclinic-web/src/test/java/pl/jiohim/petclinicguru/PetclinicguruApplicationTests.java
@@ -0,0 +1,18 @@
+package pl.jiohim.petclinicguru;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PetclinicguruApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
