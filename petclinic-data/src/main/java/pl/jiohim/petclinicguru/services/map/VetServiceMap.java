4b5fabf866cf9485b76884cb864c0b777d136766
Jiohim <bohdankrasin@gmail.com>
2022-10-15 20:29:50 +0200

    impl Map Service . closes #6

---
+++ petclinic-data/src/main/java/pl/jiohim/petclinicguru/services/map/VetServiceMap.java
@@ -0,0 +1,39 @@
+package pl.jiohim.petclinicguru.services.map;
+
+import pl.jiohim.petclinicguru.model.Owner;
+import pl.jiohim.petclinicguru.model.Vet;
+import pl.jiohim.petclinicguru.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+
+    }
+
+
+}
