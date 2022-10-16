a1d7c1a0ca715554204ff851b442b92237b5e304
Jiohim <bohdankrasin@gmail.com>
2022-10-16 17:49:43 +0200

    create Vet index page and controller . Closes #7

---
+++ petclinic-web/src/main/java/pl/jiohim/petclinicguru/controllers/VetController.java
@@ -0,0 +1,15 @@
+package pl.jiohim.petclinicguru.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets","vets/index", "vets/index.html"})
+    public String lisVets(){
+
+        return "vets/index";
+    }
+}
