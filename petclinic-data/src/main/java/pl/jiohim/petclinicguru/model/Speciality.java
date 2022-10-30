8a582d4d151c7fc5bb97699f0a2559245e47f98d
Jiohim <bohdankrasin@gmail.com>
2022-10-30 14:56:13 +0100

    add speciality Closes #28

---
+++ petclinic-data/src/main/java/pl/jiohim/petclinicguru/model/Speciality.java
@@ -0,0 +1,14 @@
+package pl.jiohim.petclinicguru.model;
+
+public class Speciality extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
