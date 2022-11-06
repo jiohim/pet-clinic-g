fae3a2d924658d8f3ebfc66c2b0694b2584683cb
Jiohim <bohdankrasin@gmail.com>
2022-11-06 17:49:45 +0100

    create SD JPA Service for owners . Closes #41

---
+++ petclinic-data/src/main/java/pl/jiohim/petclinicguru/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,66 @@
+package pl.jiohim.petclinicguru.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import pl.jiohim.petclinicguru.model.Owner;
+import pl.jiohim.petclinicguru.repositories.OwnerRepository;
+import pl.jiohim.petclinicguru.repositories.PetRepository;
+import pl.jiohim.petclinicguru.repositories.PetTypeRepository;
+import pl.jiohim.petclinicguru.services.OwnerService;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+
+
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository,
+                             PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+}
