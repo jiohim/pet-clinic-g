b81f2a322fb0521c1e3d0d59a6610551d506e03f
Jiohim <bohdankrasin@gmail.com>
2022-11-06 17:08:01 +0100

    convert entities . Closes #37

--- petclinic-data/src/main/java/pl/jiohim/petclinicguru/model/Pet.java
+++ petclinic-data/src/main/java/pl/jiohim/petclinicguru/model/Pet.java
@@ -1,12 +1,25 @@
 package pl.jiohim.petclinicguru.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
 
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @Column(name = "name")
     private String name;
 
     public String getName() {
