14ef4f4db3f4eaae9d0c58da4222980dd84ee529
Jiohim <bohdankrasin@gmail.com>
2022-11-07 16:45:33 +0100

    create SD JPA Service for Speciality . Closes #45

--- petclinic-data/src/main/java/pl/jiohim/petclinicguru/services/springdatajpa/PetSDJpaService.java
+++ petclinic-data/src/main/java/pl/jiohim/petclinicguru/services/springdatajpa/PetSDJpaService.java
@@ -1,5 +1,7 @@
 package pl.jiohim.petclinicguru.services.springdatajpa;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 import pl.jiohim.petclinicguru.model.Pet;
 import pl.jiohim.petclinicguru.repositories.PetRepository;
 import pl.jiohim.petclinicguru.services.PetService;
@@ -7,6 +9,9 @@ import pl.jiohim.petclinicguru.services.PetService;
 import java.util.HashSet;
 import java.util.Set;
 
+
+@Service
+@Profile("springdatajpa")
 public class PetSDJpaService implements PetService {
 
     private final PetRepository petRepository;
